98410873d23d9c15f0f64ef0588939736caeb7a0
hanleym <me@hanleym.com>
2025-09-15 13:19:27 -0400

    Migrated to flake-parts.

--- flake.nix
+++ flake.nix
@@ -1,32 +1,79 @@
 {
-  description = "";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
-  inputs.std.url = "github:divnix/std";
-  inputs.std.inputs.devshell.url = "github:numtide/devshell";
-  inputs.std.inputs.nixpkgs.follows = "nixpkgs";
+    crane.url = "github:ipetkov/crane";
 
-  inputs.fenix.url = "github:nix-community/fenix";
-  inputs.crane.url = "github:ipetkov/crane";
-  inputs.nixpkgs.follows = "fenix/nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    nixpkgs.follows = "fenix/nixpkgs";
 
-  outputs = inputs @ {
-    self,
-    std,
-    ...
-  }:
-    std.growOn {
-      inherit inputs;
-      systems = ["x86_64-linux" "aarch64-linux"];
-      cellsFrom = ./nix;
-      cellBlocks = with std.blockTypes; [
-        (installables "packages")
-        (devshells "shells")
-        (pkgs "rust")
-      ];
-    } {
-      devShells = std.harvest self ["core" "shells"];
-      packages = std.harvest self ["rengarde" "packages"];
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
+  };
+
+  outputs = inputs @ { self, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } (top @ { config, withSystem, moduleWithSystem, ... }: {
+    flake = {};
+
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    perSystem = perSystem @ { config, system, lib, pkgs, ... }: let
+      rustToolchain = inputs.fenix.packages.${system}.complete.withComponents [
+        "cargo"
+        "clippy"
+        "rustc"
+        "rust-src"
+        "rustfmt"
+      ];
+      craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+      commonArgs = {
+        version = if lib.hasAttr "rev" self then self.rev else self.dirtyRev;
+        src = lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            (craneLib.fileset.commonCargoSources ./.)
+          ];
+        };
+        strictDeps = true;
+        buildInputs = [];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+        pname = "rengarde-deps";
+      });
+
+      rengarde-client = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+        pname = "rengarde-client";
+        meta.mainProgram = "rengarde";
+        cargoExtraArgs = "-p client";
+        doCheck = false;
+      });
+
+      rengarde-server = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+        pname = "rengarde-server";
+        meta.mainProgram = "rengarde";
+        cargoExtraArgs = "-p server";
+        doCheck = false;
+      });
+    in {
+      devShells.default = craneLib.devShell rec {
+        buildInputs = with pkgs; [];
+        LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        NIX_RUST_TOOLCHAIN = rustToolchain;
+      };
+      packages = {
+        inherit rengarde-client rengarde-server;
+      };
+    };
+  });
 
   nixConfig = {
     extra-substituters = [
